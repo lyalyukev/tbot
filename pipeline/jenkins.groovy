495a0dd5b2f7a4171f181dd334f65974b320cfd5
lyalyukev <lyalyuk.ev@gmail.com>
2023-12-22 17:01:20 +0200

    dind

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -5,17 +5,23 @@ pipeline {
         choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
     stages {
-        stage('Example') {
+        stage('Run Docker-in-Docker') {
+            agent {
+                docker {
+                    image 'docker:19.03.12'
+                    args '-v /var/run/docker.sock:/var/run/docker.sock'
+                }
+            }
             steps {
-                echo "Build image for platform ${params.OS}"
-                echo "Build image for Arch: ${params.ARCH}"
+                script {
+                    echo "Build image for platform ${params.OS}"
+                    echo "Build image for Arch: ${params.ARCH}"
 
-                // Установка Docker внутри агента
-                sh 'curl -fsSL https://get.docker.com/ | sh'
-
-                withEnv(["TARGETOS=${params.OS}", "TARGETARCH=${params.ARCH}"]) {
-                    sh "make image"
-                    sh "make push"
+                    // Продолжение вашего кода сборки образа и пуша
+                    withEnv(["TARGETOS=${params.OS}", "TARGETARCH=${params.ARCH}"]) {
+                        sh "make image"
+                        sh "make push"
+                    }
                 }
             }
         }
